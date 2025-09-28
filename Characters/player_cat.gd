a93bc1bfe4e4a96c851be4f6cb28b6c6f15ff540
Kearney Leith <81835624+Kearskill@users.noreply.github.com>
2025-09-28 23:16:05 +0800

    tyre

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+# parameters/Idle/blend_position
+
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	animation_tree.set("parameters/Idle/blend_position", starting_direction)
 
 func _physics_process(_delta):
 	
@@ -9,8 +17,18 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	update_animation_parameters(input_direction)
+	
+	
 	# Update velocity
 	velocity = input_direction * move_speed
 	
 	# Move and Slide function usess velocitiy of character body to move character on map
 	move_and_slide()
+
+func update_animation_parameters(move_input : Vector2):
+	# don't change animation parameters if there is no input for moving
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		
